1b8149a4d33de2f04c430d226e99eda6502d2fd2
pamelaelin <sosapamelaelin@gmail.com>
2024-12-08 16:49:59 -0300

    nav fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customDarkGreen: "#165824b1",
+        customDark: "#0F0F0E",
+        customGreen: "#005A14",
+      },
+    },
   },
   plugins: [],
-}
-
+};
